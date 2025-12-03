ddb7660407955b801925487c2a68c304f0ec50c5
Yoo-JK <yoojk1025@gmail.com>
2025-12-03 04:49:13 -0600

    configs

--- slurm_scripts/agg_sph/w_sub/run_3.sh
+++ slurm_scripts/agg_sph/w_sub/run_3.sh
@@ -19,7 +19,7 @@ echo "Job started on $(date)"
 
 echo "---------- Start simulation: Aggregation 3 Sphere(s) ----------"
 cd /u/yoojk20/workspace/mnpbem_simulation
-./master.sh --str-conf ./config/agg_sph/config_str_3_agg.py --sim-conf ./config/agg_sph/config_sim_3_agg.py --verbose
+./master.sh --str-conf ./config/agg_sph/w_sub/config_str_3_agg.py --sim-conf ./config/agg_sph/w_sub/config_sim_3_agg.py --verbose
 
 echo "Job finished on $(date)"
 
--- slurm_scripts/agg_sph/w_sub/run_5.sh
+++ slurm_scripts/agg_sph/w_sub/run_5.sh
@@ -19,7 +19,7 @@ echo "Job started on $(date)"
 
 echo "---------- Start simulation: Aggregation 5 Sphere(s) ----------"
 cd /u/yoojk20/workspace/mnpbem_simulation
-./master.sh --str-conf ./config/agg_sph/config_str_5_agg.py --sim-conf ./config/agg_sph/config_sim_5_agg.py --verbose
+./master.sh --str-conf ./config/agg_sph/w_sub/config_str_5_agg.py --sim-conf ./config/agg_sph/w_sub/config_sim_5_agg.py --verbose
 
 echo "Job finished on $(date)"
 
