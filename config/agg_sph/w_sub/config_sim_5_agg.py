4fabebba5e64db9e96b2a69630e55e3ac68ac938
Yoo-JK <yoojk1025@gmail.com>
2025-12-04 16:58:03 -0600

    configs

--- config/agg_sph/w_sub/config_sim_5_agg.py
+++ config/agg_sph/w_sub/config_sim_5_agg.py
@@ -87,7 +87,7 @@ args['spectrum_xaxis'] = 'eV'
 # ADVANCED
 # ============================================================================
 args['use_mirror_symmetry'] = False
-args['use_iterative_solver'] = True
+args['use_iterative_solver'] = False
 args['use_h2_compression'] = False
 args['use_nonlocality'] = False
 
