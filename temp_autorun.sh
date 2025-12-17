e7ec5ac9a715b14cbebc8e62f44fe43a5408cf77
Yoo-JK <yoojk1025@gmail.com>
2025-12-17 11:02:26 -0600

    temp

--- temp_autorun.sh
+++ temp_autorun.sh
@@ -1,17 +1,62 @@
 #!/bin/bash
 
-module purge
-module load matlab/24.1
-module load miniconda3/24.9.2
-
-source "$(conda info --base)/etc/profile.d/conda.sh"
-conda activate mnpbem
-
 echo "Job started on $(date)"
 
-echo "---------- Start simulation: Aggregation 1 Sphere(s) ----------"
+echo "---------- Postprocess: Aggregation 1 Sphere(s) with substrate ----------"
 cd /u/yoojk20/workspace/mnpbem_simulation
-./master.sh --str-conf ./config/agg_sph/w_sub/config_str_1_agg.py --sim-conf ./config/agg_sph/w_sub/config_sim_1_agg.py --verbose
+python run_postprocess.py --str-conf ./config/agg_sph/w_sub/config_str_1_agg.py --sim-conf ./config/agg_sph/w_sub/config_sim_1_agg.py
+
+echo "---------- Postprocess: Aggregation 2 Sphere(s) with substrate ----------"
+cd /u/yoojk20/workspace/mnpbem_simulation
+python run_postprocess.py --str-conf ./config/agg_sph/w_sub/config_str_2_agg.py --sim-conf ./config/agg_sph/w_sub/config_sim_2_agg.py
+
+echo "---------- Postprocess: Aggregation 3 Sphere(s) with substrate ----------"
+cd /u/yoojk20/workspace/mnpbem_simulation
+python run_postprocess.py --str-conf ./config/agg_sph/w_sub/config_str_3_agg.py --sim-conf ./config/agg_sph/w_sub/config_sim_3_agg.py
+
+echo "---------- Postprocess: Aggregation 4 Sphere(s) with substrate ----------"
+cd /u/yoojk20/workspace/mnpbem_simulation
+python run_postprocess.py --str-conf ./config/agg_sph/w_sub/config_str_4_agg.py --sim-conf ./config/agg_sph/w_sub/config_sim_4_agg.py
+
+echo "---------- Postprocess: Aggregation 5 Sphere(s) with substrate ----------"
+cd /u/yoojk20/workspace/mnpbem_simulation
+python run_postprocess.py --str-conf ./config/agg_sph/w_sub/config_str_5_agg.py --sim-conf ./config/agg_sph/w_sub/config_sim_5_agg.py
+
+echo "---------- Postprocess: Aggregation 6 Sphere(s) with substrate ----------"
+cd /u/yoojk20/workspace/mnpbem_simulation
+python run_postprocess.py --str-conf ./config/agg_sph/w_sub/config_str_6_agg.py --sim-conf ./config/agg_sph/w_sub/config_sim_6_agg.py
+
+echo "---------- Postprocess: Aggregation 7 Sphere(s) with substrate ----------"
+cd /u/yoojk20/workspace/mnpbem_simulation
+python run_postprocess.py --str-conf ./config/agg_sph/w_sub/config_str_7_agg.py --sim-conf ./config/agg_sph/w_sub/config_sim_7_agg.py
+
+echo "---------- Postprocess: Aggregation 1 Sphere(s) without substrate ----------"
+cd /u/yoojk20/workspace/mnpbem_simulation
+python run_postprocess.py --str-conf ./config/agg_sph/wo_sub/config_str_1_agg.py --sim-conf ./config/agg_sph/wo_sub/config_sim_1_agg.py
+
+echo "---------- Postprocess: Aggregation 2 Sphere(s) without substrate ----------"
+cd /u/yoojk20/workspace/mnpbem_simulation
+python run_postprocess.py --str-conf ./config/agg_sph/wo_sub/config_str_2_agg.py --sim-conf ./config/agg_sph/wo_sub/config_sim_2_agg.py
+
+echo "---------- Postprocess: Aggregation 3 Sphere(s) without substrate ----------"
+cd /u/yoojk20/workspace/mnpbem_simulation
+python run_postprocess.py --str-conf ./config/agg_sph/wo_sub/config_str_3_agg.py --sim-conf ./config/agg_sph/wo_sub/config_sim_3_agg.py
+
+echo "---------- Postprocess: Aggregation 4 Sphere(s) without substrate ----------"
+cd /u/yoojk20/workspace/mnpbem_simulation
+python run_postprocess.py --str-conf ./config/agg_sph/wo_sub/config_str_4_agg.py --sim-conf ./config/agg_sph/wo_sub/config_sim_4_agg.py
+
+echo "---------- Postprocess: Aggregation 5 Sphere(s) without substrate ----------"
+cd /u/yoojk20/workspace/mnpbem_simulation
+python run_postprocess.py --str-conf ./config/agg_sph/wo_sub/config_str_5_agg.py --sim-conf ./config/agg_sph/wo_sub/config_sim_5_agg.py
+
+echo "---------- Postprocess: Aggregation 6 Sphere(s) without substrate ----------"
+cd /u/yoojk20/workspace/mnpbem_simulation
+python run_postprocess.py --str-conf ./config/agg_sph/wo_sub/config_str_6_agg.py --sim-conf ./config/agg_sph/wo_sub/config_sim_6_agg.py
+
+echo "---------- Postprocess: Aggregation 7 Sphere(s) without substrate ----------"
+cd /u/yoojk20/workspace/mnpbem_simulation
+python run_postprocess.py --str-conf ./config/agg_sph/wo_sub/config_str_7_agg.py --sim-conf ./config/agg_sph/wo_sub/config_sim_7_agg.py
 
 echo "Job finished on $(date)"
 
